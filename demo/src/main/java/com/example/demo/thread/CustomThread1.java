301f313ecbafdd9c8740db3ddf8bd40fb3f93f92
Kurris <791444095@qq.com>
2023-01-30 17:11:49 +0800

    remove demo

--- demo/src/main/java/com/example/demo/thread/CustomThread1.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.thread;
-
-public class CustomThread1 extends Thread {
-
-    @Override
-    public void run() {
-        System.out.println(Thread.currentThread().getName() + "is running");
-    }
-}
--- demo/src/main/java/com/example/demo/thread/CustomThread2.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.thread;
-
-public class CustomThread2 implements Runnable{
-    @Override
-    public void run() {
-        System.out.println(Thread.currentThread().getName() + "is running");
-    }
-}
