bfe373d9ba82b6b2071eec8e813975dd42f1593b
Kurris <791444095@qq.com>
2023-02-28 10:18:37 +0800

    fix 对405状态码处理

--- springclouddemo/common/src/main/java/com/example/common/filter/ExceptionFilter.java
+++ springclouddemo/common/src/main/java/com/example/common/filter/ExceptionFilter.java
@@ -3,8 +3,8 @@ package com.example.common.filter;
 import com.example.common.dto.ApiResult;
 import com.example.common.dto.ValidationErrorDto;
 import com.example.common.exception.UnauthorizedException;
-import feign.FeignException;
 import org.springframework.validation.BindException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -18,7 +18,14 @@ public class ExceptionFilter {
         return ApiResult.fail(ex);
     }
 
+    //405 处理
+    @ExceptionHandler(value = HttpRequestMethodNotSupportedException.class)
+    public void notSupportRequestMethod(HttpRequestMethodNotSupportedException ex) throws HttpRequestMethodNotSupportedException {
+        throw ex;
+    }
 
+
+    //400 处理
     @ExceptionHandler(value = BindException.class)
     public ApiResult<Object> validationExceptionHandler(BindException ex) {
         return ApiResult.fail(400, "参数验证失败", ex.getFieldErrors().stream().map(x -> ValidationErrorDto.builder()
@@ -29,6 +36,7 @@ public class ExceptionFilter {
                 .collect(Collectors.toList()));
     }
 
+    //401 处理
     @ExceptionHandler(value = UnauthorizedException.class)
     public ApiResult<Object> validationExceptionHandler(UnauthorizedException ex) {
         return ApiResult.fail(ex.getCode(), ex.getMessage());
